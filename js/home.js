7c2ef72cc1748dc6951c87cfcc5a17e8f95c54b2
KhantnK <cgm.khantnyarko@gmail.com>
2022-09-12 12:16:16 +0630

    add project

---
+++ js/home.js
@@ -0,0 +1,17 @@
+let swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
---
+++ js/review.js
@@ -0,0 +1,17 @@
+let buttons = document.querySelectorAll("button");
+let sections = document.querySelectorAll(".content");
+
+buttons.forEach((btn)=>{
+  btn.addEventListener("click", ()=>{
+    buttons.forEach((btn)=>{
+      btn.classList.remove("active");
+    });
+    btn.classList.add("active");
+    const id = btn.id;
+    sections.forEach((section)=>{
+      section.classList.remove("active");
+    });
+    const req = document.getElementsByClassName(`content${id}`);
+    req[0].classList.add("active");
+  })
+})
