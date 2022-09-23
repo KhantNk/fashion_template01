14669aa17ed5537958e9233fae48495862ef63d0
KhantnK <cgm.khantnyarko@gmail.com>
2022-09-23 16:52:52 +0630

    Initial commit

--- js/swiper.js
+++ js/swiper.js
@@ -1,25 +1,19 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+function setSwiper(direction, $class = '.swiper') {
+    return new Swiper($class, {
+        direction: direction,
+        loop: true,
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+        pagination: {
+            el: '.swiper-pagination',
+        },
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
 }
